b8441e60f1ee7dfa9076ca6d81d6420432074902
David Zmick <dpzmick@gmail.com>
2016-05-13 16:57:22 -0500

    isord

--- osr_testers/isord.c
+++ osr_testers/isord.c
@@ -5,7 +5,7 @@
 
 int isord(long* v, long n, int (*c)(void* a, void* b)) {
   for (long i=1; i<n; i++)
-    if (!c(&(v[i-1]),&(v[i]))) return 0;
+    if (!c(v+i-1,v + i)) return 0;
 
   return 1;
 }
@@ -19,12 +19,14 @@ int comp2(void *a, void *b) {
 
 int main() {
   size_t max = 1000000;
-  long arr[max]; memset(arr, 0, max*sizeof(long));
+  long *arr = malloc(sizeof(long) * max);
+  memset(arr, 0, max*sizeof(long));
 
-  void* a = malloc(1); free(a);
   printf("running isord test\n");
 
   assert(isord(arr, max, comp2) == 1); // should osr
   printf("passed\n");
+
+  free(arr);
 }
 
--- osr_testers/isord_ret1.c
+++ osr_testers/isord_ret1.c
@@ -5,7 +5,7 @@
 
 int isord(long* v, long n, int (*c)(void* a, void* b)) {
   for (long i=1; i<n; i++)
-    if (!c(&(v[i-1]),&(v[i]))) return 0;
+    if (!c(v+i-1,v + i)) return 0;
 
   return 1;
 }
@@ -16,7 +16,8 @@ int comp1(void *a, void *b) {
 
 int main() {
   size_t max = 1000000;
-  long arr[max]; memset(arr, 0, max*sizeof(long));
+  long *arr = malloc(sizeof(long) * max);
+  memset(arr, 0, max*sizeof(long));
 
   void* a = malloc(1); free(a);
   printf("running isord test\n");
@@ -24,5 +25,6 @@ int main() {
   assert(isord(arr, 100, comp1) == 1); // should not do osr
   assert(isord(arr, max, comp1) == 1); // should osr and comp -> ret 1
   printf("passed\n");
-}
 
+  free(arr);
+}
