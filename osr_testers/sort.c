bb4a983427d657dde0f47d4ceed7e29fe1cb610b
David Zmick <dpzmick@gmail.com>
2016-05-13 16:56:51 -0500

    some changes from ews machine

--- osr_testers/sort.c
+++ osr_testers/sort.c
@@ -52,14 +52,8 @@ main()
 {
 	printf("Sorting cmp1...\n");
 	sort(rand_numbers1, 0, LEN(rand_numbers1) - 1, cmp_gt);
-	for (size_t i = 0; i <  LEN(rand_numbers1); i++) {
-		printf("%zu, ", rand_numbers1[i]);
-	}
 	printf("Sorting cmp2...\n");
 	sort(rand_numbers2, 0, LEN(rand_numbers2) - 1, cmp_lt);
-	for (size_t i = 0; i <  LEN(rand_numbers2); i++) {
-		printf("%zu, ", rand_numbers2[i]);
-	}
 	return 0;
 }
 
